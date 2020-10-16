018206a7c02e6b95c751ab4a1b2a13a839bdc60b
doyeon <do83430208@gmail.com>
2020-10-16 19:52:39 +0900

    10/16 업데이트

--- BAEKJOON/2609.py
+++ BAEKJOON/2609.py
@@ -9,12 +9,12 @@ else:
     max_num = M
     min_num = max_num
 
-for i in range(2, min_num+1): #minnm = 18
+for i in range(2, min_num+1): #2부터 min_num까지 체크
     if min_num % i == 0 and max_num % i == 0:
         result_1 = i
 
 j = 1
-while True:
+while True: #max_num이 min_num으로 나누어 떨어지는 첫번째 수
     if (max_num * j) % min_num == 0:
         result_2 = max_num * j
         break
