4dbcd768b818e92bb7db73ec3bc3044afcb23841
lksy1294@gmail.com <58630483+leesh96@users.noreply.github.com>
2020-11-06 17:39:10 +0900

    2751

---
+++ Algorithm/2751.py
@@ -0,0 +1,51 @@
+import sys
+
+
+def mergesort(array):   # 병합정렬
+    if len(array) <= 1:
+        return array
+
+    mid_index = len(array) // 2
+
+    left = mergesort(array[:mid_index])     # 전반부 정렬
+    right = mergesort(array[mid_index:])    # 후반부 정렬
+    array = merge(array, left, right)       # 병합
+
+    return array
+
+
+def merge(array, left, right):  # 병합
+    p, q, t = 0, 0, 0
+
+    while p < len(left) and q < len(right):
+        if left[p] < right[q]:
+            array[t] = left[p]
+            p += 1
+        else:
+            array[t] = right[q]
+            q += 1
+        t += 1
+
+    while p < len(left):    # 왼쪽 배열 남았으면
+        array[t] = left[p]
+        t += 1
+        p += 1
+
+    while q < len(right):   # 오른쪽 배열 남았으면
+        array[t] = right[q]
+        t += 1
+        q += 1
+
+    return array
+
+
+size = int(sys.stdin.readline())
+num = []
+
+for i in range(size):
+    num.append(int(sys.stdin.readline()))
+
+num = mergesort(num)
+
+for i in num:
+    print(i)
