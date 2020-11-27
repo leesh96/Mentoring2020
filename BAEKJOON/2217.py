a3b5a2c3fc641f4a74cd8f77fd1d45ef178edd59
doyeon <do83430208@gmail.com>
2020-11-27 17:27:34 +0900

    11/27 업데이트

--- BAEKJOON/2217.py
+++ BAEKJOON/2217.py
@@ -3,9 +3,9 @@ import sys
 N = int(sys.stdin.readline())
 rope_list = [int(sys.stdin.readline()) for _ in range(N)]
 
-rope_list.sort(reverse=True)
+rope_list.sort()
 result_list = []
-for i in range(1, len(rope_list)+1):
-    result_list.append(rope_list[i-1] * i)
+for i in range(len(rope_list)):
+    result_list.append(rope_list[i] * (len(rope_list) - i))
 
 print(max(result_list))
