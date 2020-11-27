41d46f92bdf2a91ab4fa522a284079f5a182f506
doyeon <do83430208@gmail.com>
2020-11-27 21:35:30 +0900

    11/27 업데이트

--- BAEKJOON/1874.py
+++ BAEKJOON/1874.py
@@ -7,19 +7,20 @@ input_list = [int(sys.stdin.readline()) for _ in range(N)]
 stack = []
 result_list = []
 i = 1
+is_valid = True
 for target in input_list:
     while i <= target:
         stack.append(i)
         i += 1
         result_list.append('+')
     if stack[-1] > target:
-        result_list.append('NO')
+        is_valid = False
         break
     stack.pop()
     result_list.append('-')
 
 
-if 'NO' not in result_list:
+if is_valid:
     for result in result_list:
         print(result)
 else:
