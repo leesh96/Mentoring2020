6da314fc7af33d070724b5e59591005a24c7173c
doyeon <do83430208@gmail.com>
2020-12-04 10:21:45 +0900

    12/4 업데이트

---
+++ BAEKJOON/1012.py
@@ -0,0 +1,35 @@
+import sys
+sys.setrecursionlimit(50000)
+
+Test = int(sys.stdin.readline())
+def Test_f():
+###입력
+    M, N, K = map(int, sys.stdin.readline().split())
+    grid = [[0 for _ in range(N)] for _ in range(M)]
+
+    for _ in range(K):
+        x, y = (list(map(int, sys.stdin.readline().split())))
+        grid[x][y] = 1
+###
+    check_list = [[-1, 0], [1, 0], [0, 1], [0, -1]] # 탐색할때 더하고뺄 좌표
+
+    def check(x, y):
+        for i in range(4): #상하좌우 4번 반복
+            plus_x, plus_y = check_list[i]
+            if x+plus_x < 0 or x+plus_x > M-1 or y+plus_y < 0 or y+plus_y > N-1: #좌표 넘어가면 X
+                continue
+            if grid[x+plus_x][y+plus_y] == 1: #탐색한 좌표 값이 1이면 0으로 바꾸고(다시탐색X) 다시 그 좌표에 대해 check
+                grid[x+plus_x][y+plus_y] = 0
+                check(x+plus_x, y+plus_y)
+
+    earthworm = 0
+
+    for x in range(M):
+        for y in range(N):
+            if grid[x][y] == 1:
+                check(x, y)
+                earthworm += 1 #상하좌우 전부 0이면 check 반복 끝. 지렁이 += 1
+    print(earthworm)
+
+for _ in range(Test): # 테스트 수 만큼 반복
+    Test_f()
