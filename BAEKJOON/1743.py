8c243ee3390f63fd0f273afca933e21b5ed12cf1
doyeon <do83430208@gmail.com>
2020-12-21 15:24:45 +0900

    12/21 업데이트

--- BAEKJOON/1743.py
+++ BAEKJOON/1743.py
@@ -6,14 +6,24 @@ def BFS(x, y):
     queue = []
     check = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     for p_x, p_y in check:
-        if x + p_x < 0 or x + p_x >= M or y + p_y < 0 or y + p_y >= N:
-            continue
-        if grid[x + p_x][y + p_y] == 1:
-            grid[x + p_x][y + p_y] = 0
-            queue.append([x + p_x, y + p_y])
-            cnt += 1
+        if 0 <= x + p_x < M and 0 <= y + p_y < N:
+            if grid[x + p_x][y + p_y] == 1 and [x + p_x, y + p_y] not in visit:
+                visit.append([x + p_x, y + p_y])
+                queue.append([x + p_x, y + p_y])
+                cnt += 1
     for c_x, c_y in queue:
         BFS(c_x, c_y)
+    return cnt
+
+def DFS(x, y):
+    global cnt
+    check = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for p_x, p_y in check:
+        if 0 <= x + p_x < M and 0 <= y + p_y < N:
+            if grid[x + p_x][y + p_y] == 1 and [x + p_x, y + p_y] not in visit:
+                    visit.append([x + p_x, y + p_y])
+                    cnt += 1
+                    DFS(x + p_x, y + p_y)
     return cnt
 
 
@@ -23,18 +33,19 @@ grid = [[0]*N for _ in range(M)]
 
 for _ in range(K):
     y, x = map(int, sys.stdin.readline().split())
-    y -= 1
-    x -= 1
+    y -= 1 #시작좌표가 1, 1이니까 1씩 빼준다    x -= 1
     grid[x][y] = 1
 
 result = []
+visit = []
+queue = []
+
 for x in range(M):
     for y in range(N):
         cnt = 0
         if grid[x][y] == 1:
             cnt += 1
-            grid[x][y] = 0
+            visit.append([x, y])
             result.append(BFS(x, y))
 
 print(max(result))
-
