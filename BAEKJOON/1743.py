a1817954499fa565cc9d6767dc7b8f33ad6193b1
doyeon <do83430208@gmail.com>
2020-12-22 20:43:36 +0900

    12/22 업데이트

--- BAEKJOON/1743.py
+++ BAEKJOON/1743.py
@@ -1,18 +1,22 @@
 import sys
-sys.setrecursionlimit(50000)
+from collections import deque
+sys.setrecursionlimit(100000)
 
 def BFS(x, y):
     global cnt
-    queue = []
+    cnt = 1
+    queue = deque()
     check = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    for p_x, p_y in check:
-        if 0 <= x + p_x < M and 0 <= y + p_y < N:
-            if grid[x + p_x][y + p_y] == 1 and [x + p_x, y + p_y] not in visit:
-                visit.append([x + p_x, y + p_y])
-                queue.append([x + p_x, y + p_y])
-                cnt += 1
-    for c_x, c_y in queue:
-        BFS(c_x, c_y)
+    queue.append([x, y])
+
+    while queue:
+        c_x, c_y = queue.popleft()
+        for p_x, p_y in check:
+            if 0 <= x + p_x < M and 0 <= y + p_y < N:
+                if grid[c_x + p_x][c_y + p_y] == 1 and [c_x + p_x, c_y + p_y] not in visit:
+                    visit.append([c_x + p_x, c_y + p_y])
+                    queue.append([c_x + p_x, c_y + p_y])
+                    cnt += 1
     return cnt
 
 def DFS(x, y):
@@ -33,18 +37,15 @@ grid = [[0]*N for _ in range(M)]
 
 for _ in range(K):
     y, x = map(int, sys.stdin.readline().split())
-    y -= 1 #시작좌표가 1, 1이니까 1씩 빼준다    x -= 1
-    grid[x][y] = 1
+    grid[x-1][y-1] = 1
 
 result = []
 visit = []
-queue = []
 
 for x in range(M):
     for y in range(N):
         cnt = 0
         if grid[x][y] == 1:
-            cnt += 1
             visit.append([x, y])
             result.append(BFS(x, y))
 
